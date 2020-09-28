f3637f4282d5baaac35c83d2364afcc8651385f8
kamauge <kamaugerald2@gmail.com>
2020-09-28 20:30:54 +0300

    Update config.php
    changes on config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname="localhost";
-$username="root";
-$password="";
-$dbname="clearance";
+$hostname="us-cdbr-east-02.cleardb.com";
+$username="b88a372c6c809a";
+$password="b0beb5d3";
+$dbname="heroku_321bbbaa2f484f8";
 
 
 $connection = mysqli_connect($hostname,$username,$password,$dbname);
